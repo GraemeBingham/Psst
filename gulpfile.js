2e14ac7fe5c1ec2a9b7e41cc53caa748145b1657
Graeme Bingham <binghamgraeme@gmail.com>
2015-03-17 11:38:35 +1100

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	compass = require('gulp-compass');
 
 gulp.task('log', function() {
     gutil.log('Workflows are awesome!')
+});
+
+var sassSources = ['components/sass/style.scss']
+
+gulp.task('compass', function() {
+    gulp.src('sassSources')
+      .pipe(compass({
+      	sass: 'components/sass',
+      	image: 'builds/development/images',
+      	style: 'expanded' 
+      }))
+      .on('error', gutil.log)
+      .pipe(gulp.dest('builds/development/css'));
+});
+
+gulp.task('defult',['tasks'], function() {
+    // content
 });
